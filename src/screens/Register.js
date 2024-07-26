6e8cd7f75a845ea533637798653111947f39c2a2
Jackson Kirkpatrick <Kirkpatrick545454@gmail.com>
2024-07-26 16:53:36 -0500

    added register.js

---
+++ src/screens/Register.js
@@ -0,0 +1,7 @@
+import React { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '';
+import logo from '../images/commercelogo.png';
+import RequestHandler from '../components/RequestHandler.js';
+
+
