3e5ff4bbbfd3225067641103a926ca9e23ddf328
Jackson Kirkpatrick <93236436+Kirknasty54@users.noreply.github.com>
2024-08-01 21:41:15 -0500

    added full on register page. working on register functionality where the user will be prompted that their register request was sent. also got admin page to accept request and create user created

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -4,16 +4,72 @@ import logo from '../images/commercelogo.png';
 import RequestHandler from '../components/RequestHandler.js';
 import gitIcon from '../images/25231.png'
 import '../styles/Register.css'
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
 
-function Register(){
-  return(
+
+function Register() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [confirmEmail, setConfirmEmail] = useState('')
+  const [error, setError] = useState('')
+  const navigate = useNavigate();
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address.')
+      return
+    }
+  }
+
+  return (
     <div className="register-page">
-      <nav className = "navbar">
+      <nav className="navbar">
         <img src={logo} className="brand-logo" alt="logo" />
         <a href="https://github.com/Kirknasty54" >
-          <img src ={gitIcon} className="git-icon" alt="git" />
+          <img src={gitIcon} className="git-icon" alt="git" />
         </a>
       </nav>
+      <div className="register-container">
+        <form onSubmit={handleSubmit}>
+          <h2 className="register-text">Register</h2>
+          {error && <div className='error-message'>{error}</div>}
+          <div className='input-group'>
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e => setUsername(e.target.value))}
+              required
+            />
+          </div>
+          <div className='input-group'>
+            <input
+              type="email"
+              placeholder='Email'
+              value={email}
+              onChange={(e => setEmail(e.target.value))}
+              required
+            />
+          </div>
+          <div className='input-group'>
+            <input
+              type="email"
+              placeholder='Confirm Email'
+              value={confirmEmail}
+              onChange={(e => setConfirmEmail(e.target.value))}
+              required
+            />
+          </div>
+          <button type="submit" className="register-button">Register</button>
+        </form>
+      </div>
     </div>
   );
 }
